a934137d144b1d9c3265b131eedc0155e5c5cfca
Samantha Gosselin <samanthag2009@gmail.com>
2022-12-21 11:59:36 -0600

    altered js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+
 
 
 // Get references to the #generate element
@@ -20,10 +20,10 @@ var charGen = {
   numbers: '0123456789',
   symbols: '!@#$%^&*()-_=+',
 };
-
+var charSet = charGen[charType.toLowerCase()] ;
 var retVal = "";
 for (i = 0; i < length; i++) {
- retVal += charGen.charAt(Math.floor(Math.random() * length));
+ retVal += charSet.charAt(Math.floor(Math.random() * charGen.length));
 }
 return retVal;
   }
@@ -33,3 +33,9 @@ passwordText.value = password();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function copyPassword() {
+  document.getElementById("password").select();
+  navigator.clipboard.writeText(copyText.value);
+  alert("Password copied to clipboard!" + copyText.value);
+}

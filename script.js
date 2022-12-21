868840c0ef898578e6358c1f478368855db5b1d1
Samantha Gosselin <samanthag2009@gmail.com>
2022-12-21 10:48:41 -0600

    javascript started

--- script.js
+++ script.js
@@ -12,32 +12,24 @@ var length = Number(prompt("Enter the length of your password between 8-128 char
 var charType = prompt("What should we include in your password? Type 'uppercase, lowercase, numbers, symbols' to include. ")
 
   var passwordText = document.querySelector("#password");
-  var passwordGen = "";
-var charGen = charType.toLowerCase;
-if (charGen = "uppercase") {
-passwordGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-} else if (charGen = "lowercase") {
-  passwordGen = "abcdefghijklmnopqrstuvwxyz";
-} else if (charGen = "numbers") {
-  passwordGen = "0123456789";
-} else if (charGen = "symbols") {
-  passwordGen = "!@#$%^&*()-_=+";
-} else {
-  window.alert("Please type criteria for your new password!");
-}
-  passwordText.value = password;
-var password = generatePassword();
 
-//picking values out of the strings
-generatePassword: function () {
-var pick = "";
+  function password() {
+var charGen = {
+  lowercase: 'abcdefghijklmnop',
+  uppercase: 'ABCDEFGHIJKLMNOP',
+  numbers: '0123456789',
+  symbols: '!@#$%^&*()-_=+',
+};
+
+var retVal = "";
 for (i = 0; i < length; i++) {
- pick += passwordGen.charAt(Math.floor(Math.random() * length));
+ retVal += charGen.charAt(Math.floor(Math.random() * length));
 }
-return pick;
-}
+return retVal;
+  }
 
-} 
+passwordText.value = password();
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
